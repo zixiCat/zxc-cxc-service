8bf1007b3d18070650acf0f9e27a08bed1ea3b99
zixiCat <zixicat@outlook.com>
2025-01-10 18:35:34 +0800

    feat: add jOOQ dependency, enable transaction management, and implement database schema access

--- web/src/main/java/com/zxc/cxc/web/homepage/service/HomePageServiceImpl.java
+++ web/src/main/java/com/zxc/cxc/web/homepage/service/HomePageServiceImpl.java
@@ -1,15 +1,24 @@
 package com.zxc.cxc.web.homepage.service;
 
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zxc.cxc.openapi.api.HomepageApi;
 import com.zxc.cxc.openapi.common.OpenAPIResponse;
 import com.zxc.cxc.openapi.model.HomePageInfo;
+import com.zxc.cxc.web.jooq.Tables;
 
 @Service
 public class HomePageServiceImpl implements HomepageApi {
+  @Autowired
+  DSLContext dslContext;
+
   public OpenAPIResponse<HomePageInfo> homepageInfoGet() {
-    HomePageInfo homePageInfo = new HomePageInfo().intro("intro");
-    return OpenAPIResponse.ok(homePageInfo);
+    dslContext.select()
+        .from(Tables.HOME_PAGE_INFO).where(Tables.HOME_PAGE_INFO.INTRO.like("%llo%"))
+        .fetch();
+
+    return OpenAPIResponse.ok(null);
   }
 }
