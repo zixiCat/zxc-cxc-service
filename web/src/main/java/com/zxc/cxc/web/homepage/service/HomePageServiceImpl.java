0df8d4b087c4e44befd3e9c3fe8bbb26badc7f85
zixiCat <zixicat@outlook.com>
2025-01-11 01:17:32 +0800

    feat: configure MySQL datasource for admin and web modules, update OpenAPI schema references, and refactor HomePageServiceImpl

--- web/src/main/java/com/zxc/cxc/web/homepage/service/HomePageServiceImpl.java
+++ web/src/main/java/com/zxc/cxc/web/homepage/service/HomePageServiceImpl.java
@@ -1,22 +1,36 @@
 package com.zxc.cxc.web.homepage.service;
 
+import static com.zxc.cxc.jooq.generated.Tables.HOME_PAGE_INFO;
+
+// import static com.zxc.cxc.jooq.generated.Tables.HOME_PAGE_INFO;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zxc.cxc.jooq.generated.tables.records.HomePageInfoRecord;
 import com.zxc.cxc.openapi.api.HomepageApi;
 import com.zxc.cxc.openapi.common.OpenAPIResponse;
-import com.zxc.cxc.openapi.model.HomePageInfo;
-// import com.zxc.cxc.web.jooq.Tables;
+import com.zxc.cxc.openapi.model.HomePageInfoVO;
 
-@Service
-public class HomePageServiceImpl implements HomepageApi {
-  // @Autowired
-  // DSLContext dslContext;
+@Service public class HomePageServiceImpl implements HomepageApi {
+  @Autowired DSLContext dslContext;
 
-  public OpenAPIResponse<HomePageInfo> homepageInfoGet() {
-    // dslContext.select()
-    //     .from(Tables.HOME_PAGE_INFO).where(Tables.HOME_PAGE_INFO.INTRO.like("%llo%"))
-    //     .fetch();
+public
+  OpenAPIResponse<HomePageInfoVO> homepageInfoGet() {
+    var t1 = HOME_PAGE_INFO;
 
-    return OpenAPIResponse.ok(null);
+    HomePageInfoRecord homePageInfoRecord = dslContext.select()
+                                                .from(t1)
+                                                .where(t1.INTRO.like("%llo$"))
+                                                .fetchAnyInto(t1);
+
+    HomePageInfoVO homePageInfoVO =
+        new HomePageInfoVO()
+            .intro(homePageInfoRecord.getIntro())
+            .subIntro(homePageInfoRecord.getSubIntro())
+            .avatar(homePageInfoRecord.getAvatar());
+
+    return OpenAPIResponse.ok(homePageInfoVO);
   }
 }
