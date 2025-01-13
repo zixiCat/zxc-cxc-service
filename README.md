19451aed4fef1d9e3ef995e4414d086c5d1ce1f1
zixiCat <zixicat@outlook.com>
2025-01-13 10:29:05 +0800

    feat: add Docker Compose setup for MySQL 8 and initialize database with init.sql

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 - Java 17
 - Maven 3.X
 - MySQL 8.X
+
+## Init Database
+
+Running MySQL8 Service,and initialize `zixiDatabase` database using the `init.sql` script
+```sh
+docker-compose up
+```
+
+Running the Application
+```sh
+mvn clean compile
+```
--- init.sql
+++ init.sql
@@ -1,3 +1,17 @@
+DROP TABLE IF EXISTS home_page_info;
+
+CREATE TABLE
+    home_page_info (
+        id BIGINT NOT NULL COMMENT 'ID',
+        intro VARCHAR(30) NULL DEFAULT NULL COMMENT 'Intro',
+        sub_intro VARCHAR(200) NULL DEFAULT NULL COMMENT 'Sub Intro',
+        avatar VARCHAR(200) NULL DEFAULT NULL COMMENT 'Avatar',
+        create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Creation Time',
+        update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Update Time',
+        deleted TINYINT (1) NOT NULL DEFAULT 0 COMMENT 'Logical Deletion',
+        PRIMARY KEY (id)
+    );
+
 DELETE FROM home_page_info;
 
 INSERT INTO
